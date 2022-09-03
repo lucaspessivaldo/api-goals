a70f04a65a49d63b74dd000f49ae675ee1fe7002
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-09-03 12:35:56 -0300

    fix cors()

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
+app.use(cors())
 app.use('/api/goals', require('./src/routes/goalRoutes'))
 app.use('/api/user', require('./src/routes/userRoutes'))
-app.use(cors())
 
 app.get('/', (req, res) => {
   res.json({ serverStatus: "Ok" })
